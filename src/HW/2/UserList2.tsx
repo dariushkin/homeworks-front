e1c24ee46ca75acdd1af474b96bd355f435e3d7b
dariushkin <darya95kom@mail.ru>
2024-05-15 11:08:39 +0300

    homework tasks of sprint 1

---
+++ src/HW/2/UserList2.tsx
@@ -0,0 +1,31 @@
+import {CurrentUser} from './CurrentUser';
+import {UsersObjectType} from './HW2';
+
+type UserList2PropsType = {
+	users: UsersObjectType // пропиши типизацию
+	filterUsers: () => void // пропиши типизацию
+};
+
+export const UserList2 = (props: UserList2PropsType) => {
+	return (
+		<div id={'hw02-users'}>
+			<h2>User List:</h2>
+
+			<button id={'hw02-filter-button'} onClick={() => props.filterUsers()}>SHOW ME FRIENDS FROM LA</button>
+
+			<ul>
+			  {props.users.myFriends.map((user) => (
+			    <CurrentUser user={{
+					  id: user.id,
+					  name: user.name,
+					  age: user.age,
+					  address: {
+						  street: user.address.street,
+						  city: user.address.city
+					  }
+				  }} />
+			  ))}
+			</ul>
+		</div>
+	);
+};
